83cd2ec7bf160e4ab1f4ef5e6ad3151d70b5960b
Fu Cheng <alexcheng1982@gmail.com>
2017-09-13 16:10:39 +1200

    Module test

---
+++ module/src/test/java/io/vividcode/feature9/module/ModuleDescriptorTest.java
@@ -0,0 +1,30 @@
+package io.vividcode.feature9.module;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleDescriptor.Requires;
+import java.lang.module.ModuleReference;
+import java.util.function.Predicate;
+import org.junit.Test;
+
+public class ModuleDescriptorTest {
+
+	@Test
+	public void testModuleDescriptor() throws Exception {
+		final ModuleReference reference = ModuleTestSupport.getModuleReference();
+		assertNotNull(reference);
+		final ModuleDescriptor descriptor = reference.descriptor();
+		assertEquals(ModuleTestSupport.MODULE_NAME, descriptor.name());
+		assertFalse(descriptor.isAutomatic());
+		assertFalse(descriptor.isOpen());
+		assertEquals(1, descriptor.exports().size());
+		assertEquals(1, descriptor.packages().size());
+		assertTrue(descriptor.requires().stream().map(Requires::name).anyMatch(Predicate.isEqual("jackson.core")));
+		assertTrue(descriptor.uses().isEmpty());
+		assertTrue(descriptor.provides().isEmpty());
+	}
+}
---
+++ module/src/test/java/io/vividcode/feature9/module/ModuleTestSupport.java
@@ -0,0 +1,19 @@
+package io.vividcode.feature9.module;
+
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+public class ModuleTestSupport {
+
+	public static final String MODULE_NAME = "io.vividcode.store.common";
+
+	public static ModuleFinder getModuleFinder() throws URISyntaxException {
+		return ModuleFinder.of(Paths.get(ModuleDescriptorTest.class.getResource("/modules").toURI()));
+	}
+
+	public static ModuleReference getModuleReference() throws URISyntaxException {
+		return getModuleFinder().find(MODULE_NAME).get();
+	}
+}
