3f5e3f9cbfa2027fbb69ef7f489729f39c9e2601
Fu Cheng <alexcheng1982@gmail.com>
2017-09-19 16:18:51 +1200

    Module resources test

--- module/src/test/java/io/vividcode/feature9/module/ResourceTest.java
+++ module/src/test/java/io/vividcode/feature9/module/ResourceTest.java
@@ -1,12 +1,23 @@
 package io.vividcode.feature9.module;
 
+import static org.junit.Assert.assertTrue;
+
+import java.net.URISyntaxException;
+import java.util.Optional;
 import org.junit.Test;
 
 public class ResourceTest {
 
-  @Test
-  public void testResources() {
-    final ClassLoader classLoader = ResourceTest.class.getClassLoader();
-    classLoader.resources("res/application.properties").forEach(System.out::println);
-  }
+	@Test
+	public void testResources() throws URISyntaxException {
+		final Optional<Module> moduleOpt = ModuleTestSupport.getModule();
+		assertTrue(moduleOpt.isPresent());
+		final Module module = moduleOpt.get();
+		assertTrue(module.isOpen("config"));
+		assertTrue(module
+				.getClassLoader()
+				.resources("config/application.properties")
+				.count() > 0
+		);
+	}
 }
