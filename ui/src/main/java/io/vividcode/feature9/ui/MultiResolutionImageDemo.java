346b6bbc3cf761ca574fcaa8b5b4f77bab915bde
Fu Cheng <alexcheng1982@gmail.com>
2017-09-27 13:00:00 +1300

    UI samples

--- ui/src/main/java/io/vividcode/feature9/ui/MultiResolutionImageDemo.java
+++ ui/src/main/java/io/vividcode/feature9/ui/MultiResolutionImageDemo.java
@@ -23,19 +23,19 @@ public class MultiResolutionImageDemo {
           .getImage(
               MultiResolutionImageDemo.class
                   .getResource(String.format("/images/%s", this.name)));
-      g.drawImage(image, 0, 0, this);
+      g.drawImage(image, 10, 10, this);
       g.dispose();
     }
   }
 
   public static void main(final String[] args) {
     final JFrame frame = new JFrame();
+    frame.setTitle("Multi-resolution images");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new BorderLayout());
     final ImageCanvas canvas = new ImageCanvas("flower.png");
-    canvas.setSize(700, 500);
     frame.add("Center", canvas);
-    frame.setSize(700, 500);
+    frame.setSize(360, 300);
     frame.setVisible(true);
   }
 }
